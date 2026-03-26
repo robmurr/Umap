5b7e03d71d1e10a513ba733f9856d4662312392d
LohmanJoe <JLohman1993@gmail.com>
2026-03-25 23:58:52 -0400

    Initial Google Maps project setup

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.7749,
+          longitude: -122.4194,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
